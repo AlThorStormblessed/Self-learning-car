5e6e1a27f046cdb3bf171ee532305fc7a0d9398e
AlThorStormblessed <anshgudibanda@gmail.com>
2024-04-03 16:48:08 +0530

    Final remote version

--- remote_car.py
+++ remote_car.py
@@ -1,60 +1,104 @@
-from gpiozero import Motor
-import curses
+import RPi.GPIO as GPIO          
+from time import sleep
 
-lmotor = Motor(forward=16, backward=17)
-rmotor = Motor(forward=18, backward=13)
+in1 = 24
+in2 = 23
+en = 25
 
-def left():
-#    print('Left ...')
-    lmotor.backward()
-    rmotor.forward()
+in3 = 22
+in4 = 27
+en2 = 17
+temp1=1
 
-def right():
-#    print('Right ...')
-    lmotor.forward()
-    rmotor.backward()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(en2,GPIO.OUT)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+p=GPIO.PWM(en,1000)
+p.start(100)
+p2 = GPIO.PWM(en2, 1000)
+p2.start(100)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")    
 
-def forward():
-#    print('Forwarding ...')
-    lmotor.forward()
-    rmotor.forward()
+while(1):
 
-def reverse():
-#    print('Reversing ...')
-    lmotor.backward()
-    rmotor.backward()
-
-def stop():
-#    print('Stopping ...')
-    lmotor.stop()
-    rmotor.stop()
-
-actions = {
-    curses.KEY_UP:    forward,
-    curses.KEY_DOWN:  reverse,
-    curses.KEY_LEFT:  left,
-    curses.KEY_RIGHT: right,
-}
-
-def main(window):
-    next_key = None
-    while True:
-        curses.halfdelay(1)
-        if next_key is None:
-            key = window.getch()
+    x=input()
+    
+    if x=='r':
+        print("run")
+        if(temp1==1):
+         GPIO.output(in1,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in3,GPIO.HIGH)
+         GPIO.output(in4,GPIO.LOW)
+         print("forward")
+         x='z'
         else:
-            key = next_key
-            next_key = None
-        if key != -1:
-            # KEY PRESSED
-            curses.halfdelay(3)
-            action = actions.get(key)
-            if action is not None:
-                action()
-            next_key = key
-            while next_key == key:
-                next_key = window.getch()
-            # KEY RELEASED
-            stop()
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+         GPIO.output(in3,GPIO.LOW)
+         GPIO.output(in4,GPIO.HIGH)
+         print("backward")
+         x='z'
 
-curses.wrapper(main)
+    elif x=='x':
+        print("stop")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
+        x='z'
+
+    elif x=='w':
+        print("forward")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
+        temp1=1
+        x='z'
+
+    elif x=='s':
+        print("backward")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
+        temp1=0
+        x='z'   
+        
+    elif x=='a':
+        print("left")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
+        temp1=0
+        x='z'   
+        
+    elif x=='d':
+        print("right")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
+        temp1=0
+        x='z'   
+    
+    elif x=='e':
+        GPIO.cleanup()
+        break
+    
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
